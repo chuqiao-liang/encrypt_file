cae7e8195b3f3a6975b9521d0bde27b65b3c529d
Carol <35831889+chuqiao-liang@users.noreply.github.com>
2019-05-18 13:22:10 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileBox = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_browse = new System.Windows.Forms.Button();
+            this.button_decrypt = new System.Windows.Forms.Button();
+            this.button_encrypt = new System.Windows.Forms.Button();
+            this.openDlg = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // fileBox
+            // 
+            this.fileBox.Location = new System.Drawing.Point(32, 76);
+            this.fileBox.Name = "fileBox";
+            this.fileBox.Size = new System.Drawing.Size(417, 26);
+            this.fileBox.TabIndex = 0;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(32, 160);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(153, 26);
+            this.keyBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Key:";
+            // 
+            // button_browse
+            // 
+            this.button_browse.Location = new System.Drawing.Point(473, 72);
+            this.button_browse.Name = "button_browse";
+            this.button_browse.Size = new System.Drawing.Size(83, 34);
+            this.button_browse.TabIndex = 4;
+            this.button_browse.Text = "Browse";
+            this.button_browse.UseVisualStyleBackColor = true;
+            this.button_browse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // button_decrypt
+            // 
+            this.button_decrypt.Location = new System.Drawing.Point(174, 235);
+            this.button_decrypt.Name = "button_decrypt";
+            this.button_decrypt.Size = new System.Drawing.Size(97, 34);
+            this.button_decrypt.TabIndex = 5;
+            this.button_decrypt.Text = "Decrypt";
+            this.button_decrypt.UseVisualStyleBackColor = true;
+            this.button_decrypt.Click += new System.EventHandler(this.button_decrypt_Click);
+            // 
+            // button_encrypt
+            // 
+            this.button_encrypt.Location = new System.Drawing.Point(33, 235);
+            this.button_encrypt.Name = "button_encrypt";
+            this.button_encrypt.Size = new System.Drawing.Size(101, 34);
+            this.button_encrypt.TabIndex = 6;
+            this.button_encrypt.Text = "Encrypt";
+            this.button_encrypt.UseVisualStyleBackColor = true;
+            this.button_encrypt.Click += new System.EventHandler(this.button_encrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 319);
+            this.Controls.Add(this.button_encrypt);
+            this.Controls.Add(this.button_decrypt);
+            this.Controls.Add(this.button_browse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.fileBox);
+            this.Name = "Form1";
+            this.Text = "File Encrypt/Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileBox;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_browse;
+        private System.Windows.Forms.Button button_decrypt;
+        private System.Windows.Forms.Button button_encrypt;
+        private System.Windows.Forms.OpenFileDialog openDlg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        string errormissingfile = "Could not open source or destination file.";
+        string errormissingkey = "Please enter a key.";
+        string errornotenc = "Not a .enc file.";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)
+        {
+            if (openDlg.ShowDialog(this) == DialogResult.OK)
+                fileBox.Text = openDlg.FileName;
+        }
+
+        private void button_encrypt_Click(object sender, EventArgs e)
+        {
+            // first check for valid file and key.
+            bool fileexist = File.Exists(fileBox.Text);
+            if (keyBox.Text == "")
+            {
+                MessageBox.Show(errormissingkey, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            } else if (!fileexist)
+            {
+                MessageBox.Show(errormissingfile, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int rbyte;
+            int pos = 0;    //position in key string
+            int length = keyBox.Text.Length; //length of key
+            byte kbyte, ebyte; //encrypted byte
+
+            string message = "Operation completed successfully.";
+            MessageBoxButtons buttonOK = MessageBoxButtons.OK;
+            FileStream fin = new FileStream(fileBox.Text, FileMode.Open); // reading form this file
+            FileStream fout = new FileStream(fileBox.Text + ".enc", FileMode.Create);
+
+            while ((rbyte = fin.ReadByte()) != -1)
+            {
+                kbyte = (byte)keyBox.Text[pos];
+                ebyte = (byte)(rbyte ^ kbyte);
+                fout.WriteByte(ebyte);
+                ++pos;
+                if (pos == length)
+                    pos = 0;
+            }
+
+            DialogResult result;
+            result = MessageBox.Show(message, "", buttonOK);
+            fin.Close();
+            fout.Close();
+        }
+
+        private void button_decrypt_Click(object sender, EventArgs e)
+        {
+            // first check for valid file and key.
+            bool fileexist = File.Exists(fileBox.Text);
+
+            if (keyBox.Text == "")
+            {
+                MessageBox.Show(errormissingkey, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (!fileexist)
+            {
+                MessageBox.Show(errormissingfile, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string substr = fileBox.Text.Substring(fileBox.Text.Length - 4);
+            if (substr != ".enc")
+            {
+                MessageBox.Show(errornotenc, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int rbyte;
+            int pos = 0;    //position in key string
+            int length = keyBox.Text.Length; //length of key
+            byte kbyte, ebyte; //encrypted byte
+           
+            string undoenc = fileBox.Text.Replace(".enc", "");
+            string overwrite = "Output file exists. Overwrite?";
+            MessageBoxButtons buttonYesNo = MessageBoxButtons.YesNo;
+            bool fileexistenc = File.Exists(undoenc);
+            DialogResult dialogresult = DialogResult.No;
+
+            if (fileexistenc)
+            {
+                dialogresult = MessageBox.Show(overwrite, "", buttonYesNo, MessageBoxIcon.Question);
+            }
+
+            if (dialogresult == DialogResult.Yes || !fileexistenc)
+            {
+                FileStream fin = new FileStream(fileBox.Text, FileMode.Open); // reading form this file
+                FileStream fout = new FileStream(undoenc, FileMode.Create);
+
+                while ((rbyte = fin.ReadByte()) != -1)
+                {
+                    kbyte = (byte)keyBox.Text[pos];
+                    ebyte = (byte)(rbyte ^ kbyte);
+                    fout.WriteByte(ebyte);
+                    ++pos;
+                    if (pos == length)
+                        pos = 0;
+                }
+                DialogResult result;
+                result = MessageBox.Show("Operation completed successfully.", "", MessageBoxButtons.OK);
+
+                fin.Close();
+                fout.Close();
+            }
+            else if (dialogresult == DialogResult.No) return;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
